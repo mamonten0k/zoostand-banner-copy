b21d60044dfe61baecfac9b4287c454eaa9d3bf8
dozy_owl <lobanvwrk18@gmail.com>
2023-07-23 11:17:59 +0300

    init(*): create banner

---
+++ demo/src/components/Details.jsx
@@ -0,0 +1,14 @@
+export default function Details() {
+  return (
+    <>
+      <nav className="nav">
+        <a href="#" children="Гильдия" />
+        <a href="#" children="Профиль" />
+      </nav>
+      <div className="bottom">
+        <a href="#" className="bottom-left" children="madeby@lobanovworkya" />
+      </div>
+      <span className="header-left">Castlemania</span>
+    </>
+  )
+}
